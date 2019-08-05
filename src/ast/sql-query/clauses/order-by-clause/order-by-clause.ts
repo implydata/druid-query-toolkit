83c669fb765d2ac4c9febb148c40b785246334f8
Vadim Ogievetsky <vadimon@gmail.com>
2019-08-04 23:54:15 -0700

    updated copy rights

--- src/ast/sql-query/clauses/order-by-clause/order-by-clause.ts
+++ src/ast/sql-query/clauses/order-by-clause/order-by-clause.ts
@@ -1,11 +1,7 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -16,7 +12,7 @@
  * limitations under the License.
  */
 
-import { Columns } from '../../../../../build/ast';
+import { Columns } from '../columns/columns';
 import { OrderByPart, StringType } from '../../../index';
 
 export interface OrderByClauseValue {
--- src/ast/sql-query/clauses/order-by-clause/order-by-part.ts
+++ src/ast/sql-query/clauses/order-by-clause/order-by-part.ts
@@ -1,11 +1,7 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -17,8 +13,9 @@
  */
 
 import { CaseExpression, Function, Integer, RefExpression, StringType } from '../../..';
-import { Columns } from '../../../../../build/ast';
 import { getColumns } from '../../helpers';
+import { Columns } from '../columns/columns';
+
 export interface OrderByPartValue {
   direction: string | null;
   orderBy: StringType | RefExpression | Integer | Function | CaseExpression;
