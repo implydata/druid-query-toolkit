7d070f4a60626b87cc22fd2540341813575971b9
Maggie Brewster <maggiebrewster@implydata20sMBP.attlocal.net>
2020-02-12 15:42:49 -0800

    remove old functions

--- src/parser/druidsql.header.js
+++ src/parser/druidsql.header.js
@@ -4,20 +4,6 @@
     return [head].concat(tail.map(function(t) { return t[1] }));
   }
 
-  function makeListMap(head, tail) {
-    if (head == null) return [];
-    return [head].concat(tail.map(function(t) { return t }));
-  }
-
-  function makeListMap_1(head, tail) {
-    if (head == null) return [];
-    return [head].concat(tail.map(function(t) { return t[0][1] }));
-  }
-  function makeListMap_2(head, tail) {
-    if (head == null) return [];
-    return [head].concat(tail.map(function(t) { return t[0][2] }));
-  }
-
   function makeListMapEmpty(tail) {
     return [].concat(tail.map(function(t) { return t[1] }));
   }
@@ -26,34 +12,9 @@
     return [].concat(tail.map(function(t) { return t[0] }));
   }
 
-  function makeListMapEmpty0BookEnd(tail) {
-    return [].concat(tail.map(function(t) { return [t[0][0],t[0][2]] })).flat();
-  }
-
-  function makeListMapEmpty0Joined(tail) {
-    return [].concat(tail.map(function(t) { return t[0].join('') }));
-  }
-
-  function makeListMapEmptyConcat0(tail) {
-    return tail.map(function(t) { return t[0].join('') });
-  }
-
-  function makeListMapEmpty01(tail) {
-    return [].concat(tail.map(function(t) { return t[0][1] }));
-  }
-
-  function makeListCasesSpacing(caseValue) {
-    caseValue.map(caseValue => {caseValue[1].spacing = makeListMap(caseValue[0], caseValue[1].spacing)});
-    return makeListMapEmpty(caseValue);
-  }
-
   function makeListMap3(head, tail) {
     if (head == null) return [];
     return [head].concat(tail.map(function(t) { return t[3] }));
-  }
-
-  function makeEmptyListMap3(tail) {
-    return tail.map(function(t) { return t[3] });
   }
 
   function makeSeparatorsList(tail) {
@@ -62,24 +23,6 @@
       left: t[2],
       separator: t[1],
     } ;}));
-  }
-
-  ///this is really shitty
-  function makeUnarySeparatorsList(head, tail) {
-    if (head == null) return [];
-    return [{
-      rightSpace: head[1],
-      separator: head[0],}].concat((tail.map(t =>  {
-        if(tail.length === 3){
-      return {left: t[0],
-      right: t[2],
-      separator: t[1]}
-        } 
-    })));
-  }
-
-  function makeUnaryArgumentsList(head, tail) {
-    return [head[2]].concat(tail.map(function(t) { return t[3] }));
   }
 
   let functions;
