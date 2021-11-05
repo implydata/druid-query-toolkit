65d838922d5e49fbb468e151381e1c64c6ac25ff
Vadim Ogievetsky <vadim@ogievetsky.com>
2021-11-05 15:28:51 -0700

    depricated marker

--- src/sql/sql-expression.ts
+++ src/sql/sql-expression.ts
@@ -134,6 +134,13 @@ export abstract class SqlExpression extends SqlBase {
     return SqlOrderByExpression.create(this, direction);
   }
 
+  /**
+   * @deprecated use toOrderByExpression instead
+   */
+  public toOrderByPart(direction?: SqlOrderByDirection): SqlOrderByExpression {
+    return this.toOrderByExpression(direction);
+  }
+
   // SqlComparison
 
   public equal(rhs: SqlExpression | LiteralValue): SqlComparison {
